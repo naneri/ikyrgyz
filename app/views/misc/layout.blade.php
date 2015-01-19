2c6997d8880fee7c272b1166f1f83403a3eb6ad4
naneri <ktnaneri@gmail.com>
2015-01-19 16:53:48 +0600

    продолжил интеграцию codesleeve

--- app/views/misc/layout.blade.php
+++ app/views/misc/layout.blade.php
@@ -1,17 +1,16 @@
 <html>
 	<head>
-		 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<link href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
-		<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-                <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
-                <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-                <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-		
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+        
+		<?= stylesheet_link_tag() ?>
+    	
 	</head>
 	<body>
 		@include('misc.navbar')
 		@yield('content')
-                
-                @yield('scripts')
+        @yield('scripts')
+
+     	<?= javascript_include_tag() ?>
+     	<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
 	</body>
 </html>
--- app/views/misc/navbar.blade.php
+++ app/views/misc/navbar.blade.php
@@ -1,81 +1,52 @@
 @if(Auth::check())
+	<div class="b-header">
+      <div class="b-header__inner">
+        <div class="b-header__logo"><a href="{{URL::to('/')}}"><img src="{{URL::to('/img')}}/content/logo__image.png" alt=""/><span>I-KYRGYZ</span></a></div>
+        <div class="b-header__menu"><a href="#">&#9776;</a></div>
+        <div class="b-header__navigation">
+          <ul class="menu dropit">
+            <li class="dropit-trigger dropit-open"><a href="#"><img src="{{URL::to('/img')}}/content/user-name.png" alt=""/><span>Имя пользователя</span></a>
+              <ul class="dropit-submenu dropdown dropdown-margin">
+                <li><a href="{{URL::to('profile/edit')}}">Изменить профиль</a></li>
+                <li><a href="{{URL::to('message/all')}}">Личные сообщения(1877)</a></li>
+                <li><a href="{{URL::to('profile/friends')}}">Друзья</a></li>
+                <li><a href="">Группы</a></li>
+              </ul>
+            </li>
+            <li><a href="#"><img src="{{URL::to('/img')}}/content/message.png" alt=""/><span>25</span></a></li>
+            <li><a href="#"><img src="{{URL::to('/img')}}/content/friends.png" alt=""/><span>25</span></a></li>
+            <li><a href="#"><img src="{{URL::to('/img')}}/content/setting2.png" alt=""/><span>25</span></a></li>
+            <li><a href="{{URL::to('people')}}"><img src="{{URL::to('/img')}}/content/search-btn.png" alt=""/><span>Поиск друзей</span></a></li>
+            <li class="enc"><a href="#"><img src="{{URL::to('/img')}}/content/enc.png" alt=""/><span>Энциклопедия</span></a>
+              <ul class="dropdown">
+                <li><a href="">item</a></li>
+                <li><a href="">item</a></li>
+                <li><a href="">item	 </a></li>
+              </ul>
+            </li>
+            <li><a href="#"><img src="{{URL::to('/img')}}/content/ask.png" alt=""/></a>
+              <ul class="dropdown">
+                <li><a href="">item</a></li>
+                <li><a href="">item</a></li>
+                <li><a href="">item	 </a></li>
+              </ul>
+            </li>
+            <li class="flag"><a href="#"><img src="{{URL::to('/img')}}/content/flag.png" alt=""/></a>
+              <ul class="dropdown">
+                <li><a href=""> Lorem ipsum dolor sit amet.</a></li>
+                <li><a href="">item</a></li>
+                <li><a href="">item</a></li>
+              </ul>
+            </li>
+            <li><a href="{{URL::to('/logout')}}"><img src="{{URL::to('/img')}}/content/close.png" alt=""/></a></li>
+          </ul>
+        </div>
+        <div class="clear"></div>
+      </div>
+    </div>
+
+
 	
-	<nav class="navbar navbar-inverse">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="{{url()}}">I-Kyrgyz</a>
-			</div>
-			<div id="navbar" class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					<li class="dropdown">
-		                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-		                <ul class="dropdown-menu" role="menu">
-		                  <li><a href="{{URL::to('profile/edit')}}">Edit Profile</a></li>
-		                  <li><a href="{{URL::to('profile/friends')}}">Друзья</a></li>
-		                  <li><a href="{{URL::to('message/all')}}">Личные сообщения</a></li>
-		                  <li class="divider"></li>
-		                  <li class="dropdown-header">Nav header</li>
-		                  <li><a href="#">aaa</a></li>
-		                  <li><a href="#">aa</a></li>
-		                </ul>
-		            </li>
-		             <li class="dropdown">
-		            	<a href="" class="dropdown-toggle" data-toggle='dropdown'>Новые Сообщения<span class="caret"></span></a>
-		            	<ul class="dropdown-menu">
-                                    @if(isset($new_messages))
-		            		@foreach($new_messages as $message)
-		            		<li><a href="{{URL::to('message/show/'.$message->id)}}">{{$message->email}} | {{$message->text}}</a></li>
-		            		@endforeach
-                                    @endif
-		            	</ul>
-		            </li>
-					<li class="dropdown">
-		                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Запросы в друзья<span class="caret"></span></a>
-		                @if(isset($friend_requests))
-							<ul class="dropdown-menu" role="menu">
-		                  		@foreach($friend_requests as $request)
-		                  			<li>{{link_to('people/submitFriend/'.$request->user_two, $request->email)}}</li>
-		                  		@endforeach
-		                	</ul>
-		                @endif
-		            </li>
-
-		            
-
-		            
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="{{URL::to('people')}}">{{trans('network.search_friends')}}</a></li>
-
-		            <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Энциклопедия<span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{URL::to('custom/history')}}">История</a></li>
-                            <li><a href="{{URL::to('custom/customs')}}">Обычаи</a></li>
-                            <li><a href="{{URL::to('custom/culture')}}">Культура</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Помощь<span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{URL::to('custom/help')}}">Помощь</a></li>
-                            <li><a href="{{URL::to('custom/problem')}}">Сообщить о проблеме</a></li>
-                            <li><a href="{{URL::to('custom/action_history')}}">История действий</a></li>
-                        </ul>
-                    </li>
-					<li><a href="{{URL::to('logout')}}">{{trans('network.logout')}}</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
 
 @endif
 
