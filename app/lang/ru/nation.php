8f88be59a8a7c03c082e755b126695470795c306
naneri <ktnaneri@gmail.com>
2015-03-09 13:29:18 +0600

    добавили армян

--- app/lang/ru/nation.php
+++ app/lang/ru/nation.php
@@ -1,10 +1,12 @@
 <?php
 
 return array(
-	'kyrgyz_adjective' 	=> 'Кыргызская',
-	'kazakh_adjective' 	=> 'Казахская',
-	'russian_adjective' => 'Русская',
-	'uyghur_adjective' 	=> 'Уйгурская',
-	'uzbek_adjective' 	=> 'Узбекская',
-	'chechen_adjective' => 'Чеченская',
+	'kyrgyz_adjective' 		=> 'Кыргызская',
+	'kazakh_adjective' 		=> 'Казахская',
+	'russian_adjective' 	=> 'Русская',
+	'uyghur_adjective' 		=> 'Уйгурская',
+	'uzbek_adjective' 		=> 'Узбекская',
+	'chechen_adjective' 	=> 'Чеченская',
+	'armenian_adjective' 	=> 'Армянская',
+
 	);
