342a261d89b16f86e706230465bac668031fc030
beks_torokulov <beks.torokulov@gmail.com>
2015-03-31 11:06:04 +0400

    main page

--- app/views/misc/createnav.blade.php
+++ app/views/misc/createnav.blade.php
@@ -54,7 +54,7 @@
         <div class="b-topic-navigation__right">
             <ul class="b-topic-navigation-format">
                 <li class="b-topic-navigation-format__list">
-                    <a href="#" class="format-icon"><img src="{{ asset('img/42.png') }}" alt=""/></a>
+                    <a href="#" class="format-icon" style="display: block; margin: 5px 0 0 5px;"><img src="{{ asset('img/42.png') }}" alt=""/></a>
                     <ul class="b-topic-navigation-format-dropdown">
                         <li><a href="javascript: makeColumnN('1')"><img src="{{ asset('img/44.png') }}" alt=""/></a></li>
                         <li><a href="javascript: makeColumnN('2')"><img src="{{ asset('img/45.png') }}" alt=""/></a></li>
