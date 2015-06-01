114ec0d9e2fcb38e6f3f888985ab53185d441d45
yrysbek <respect.page@gmail.com>
2015-06-01 17:44:48 +0600

    подписки чужого профиля

--- app/views/profile/show/user.blade.php
+++ app/views/profile/show/user.blade.php
@@ -119,62 +119,12 @@
             <li class="b-user-navigation-list__list tab-link current" data-tab="tab-1" ><a href="{{URL::to('profile/'.$user->id)}}">Лента</a></li>
             <li class="b-user-navigation-list__list" data-tab="tab-4"><a href="{{URL::to('profile/'.$user->id.'/publications')}}">Публикации</a><span>{{$user->publications(10000)->count()}}</span></li>
             <li class="b-user-navigation-list__list" data-tab="tab-3"><a href="{{URL::to('profile/'.$user->id.'/friends')}}">Друзья</a><span>{{$user->friends()->count()}}</span></li>
-            <li class="b-user-navigation-list__list"><a href="{{URL::to('profile/'.$user->id.'/subscribtions')}}">Подписано</a></li>
+            <li class="b-user-navigation-list__list"><a href="{{URL::to('profile/'.$user->id.'/subscriptions')}}">Подписано</a><span>{{$user->subscriptions()->count()}}</span></li>
             <div class="clear"></div>
         </ul>
     </div>
 </div>
 
 @include('profile.show.body', compact('items', 'page'))
-</div>
-@stop
-
-@section('content-old')
-
-
-<div class="b-content">
-    <div class="b-user-profile">
-        <div class="b-user-profile__left">
-            <a href="{{asset($user->avatar())}}" class="user-image">
-                <img src="{{asset($user->avatar())}}" style="width: 244px; height: 244px;" />
-            </a>
-        </div>
-        <script>
-            $(document).ready(function(){
-                $('.b-user-profile .b-user-profile__left').photobox('a', {thumbs: false, autoplayBtn: false,});
-            });
-        </script>
-
-        <div class="b-user-profile__middle">
-            @include('profile.show.info', compact('user', 'gender', 'marital_status'))
-        </div>
-        <div class="b-user-profile__right">
-            <ul class="b-user-profile-buttons">
-                @if(!$friend_status)
-                    <li class="b-user-profile-buttons__list"><a href="{{URL::to('people/friendRequest/'.$user->id)}}">{{ trans('network.add-to-friends') }}<span class="search-image"></span></a></li>
-                @else
-                    <li class="b-user-profile-buttons__list"><a href="{{URL::to('messages/new?receiver='.$user->getNames())}}">{{ trans('network.send-message') }}<span class="msg-image"></span></a></li>
-                @endif
-                <li class="b-user-profile-buttons__list"><a style="height: 55px;line-height: 55px;" href="{{URL::to('profile/random')}}">{{ trans('network.random-profile') }}<span class="random-image"></span></a></li>
-            </ul>
-        </div>
-        <div class="clear"></div>
-    </div>
-    <div class="b-user-navigation">
-        <ul class="b-user-navigation-list">
-            <li class="b-user-navigation-list__list"><a href="{{URL::to('profile/'.$user->id.'/newsline')}}">{{ trans('network.timeline') }}</a></li>
-            <li class="b-user-navigation-list__list"><a href="{{URL::to('profile/'.$user->id.'/publications')}}">{{ trans('network.publications') }}</a><span>{{$user->publications(10000)->count()}}</span></li>
-            <li class="b-user-navigation-list__list"><a href="{{URL::to('profile/'.$user->id.'/friends')}}">{{ trans('network.friends') }}</a><span>{{$user->friends()->count()}}</span></li>
-            <li class="b-user-navigation-list__list"><a href="{{URL::to('profile/'.$user->id.'/subscriptions')}}">{{ trans('network.subscriptions') }}</a></li>
-            <div class="clear"></div>
-        </ul>
-    </div>
-
-    @include('profile.show.body', compact('items', 'page'))
-
-    <div class="clear"></div>
-</div>
-</div>
-
 </div>
 @stop
