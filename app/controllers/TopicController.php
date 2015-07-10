4f5823b1705b88fa333af5c72cb562de143c4fd7
naneri <ktnaneri@gmail.com>
2015-07-10 16:59:54 +0600

    новый фикс

--- app/controllers/TopicController.php
+++ app/controllers/TopicController.php
@@ -284,7 +284,7 @@ class TopicController extends BaseController {
         if (Session::get('topicCover')) {
 
             //checking if directory exists and creating if it does not
-            $directory = "images/{$this->topic->blog_id}/{$this->topic->id}";
+            $directory = 'images/' . $this->topic->blog_id . '/' . $this->topic->id;
             if(!File::isDirectory($directory)){
                 File::makeDirectory($directory,  $mode = 0777, $recursive = true);
             }
